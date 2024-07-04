d3d0d71d79c5c213b1b600cb8811dbf858ac4d31
Vikas Raj Yadav <vikasyadav.vision111@gmail.com>
2024-07-05 03:26:08 +0530

    Initial Commit

---
+++ flippd/flippdApp.swift
@@ -0,0 +1,17 @@
+//
+//  flippdApp.swift
+//  flippd
+//
+//  Created by Vikas Yadav on 05/07/24.
+//
+
+import SwiftUI
+
+@main
+struct flippdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ flippdTests/flippdTests.swift
@@ -0,0 +1,17 @@
+//
+//  flippdTests.swift
+//  flippdTests
+//
+//  Created by Vikas Yadav on 05/07/24.
+//
+
+import Testing
+@testable import flippd
+
+struct flippdTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
