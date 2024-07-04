926c6a6a6311ba59e9dcca481108277f60b6a949
Vikas Raj Yadav <vikasyadav.vision111@gmail.com>
2024-07-05 03:41:21 +0530

    Added Toggle, Z Stacks and other cool stuff.

--- flippd/ContentView.swift
+++ flippd/ContentView.swift
@@ -9,15 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: true)
         }
+        .foregroundColor(.orange)
         .padding()
     }
 }
+
+
+struct CardView: View  {
+    @State var isFaceUp: Bool
+    
+    var body : some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            }
+            else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        }.onTapGesture() {
+            isFaceUp.toggle()
+            print("Tapped")
+        }
+    }
+}
+
+
+
 
 #Preview {
     ContentView()
